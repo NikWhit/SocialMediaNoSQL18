30efeca753c636bf231d582fa466c241cd27f541
NikWhit <135679785+NikWhit@users.noreply.github.com>
2023-12-13 01:05:47 -0700

    upload after 2nd time building

--- index.js
+++ index.js
@@ -3,6 +3,34 @@ const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const http = require("http");
+
+const options = {
+  "method": "GET",
+  "hostname": "localhost",
+  "port": "3001",
+  "path": "/",
+  "headers": {
+    "Content-Type": "application/json",
+    "User-Agent": "Insomnia/2023.5.6",
+    "Content-Length": "0"
+  }
+};
+
+const req = http.request(options, function (res) {
+  const chunks = [];
+
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on("end", function () {
+    const body = Buffer.concat(chunks);
+    console.log(body.toString());
+  });
+});
+
+req.end();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
